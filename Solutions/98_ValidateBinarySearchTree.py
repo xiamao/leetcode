31fe650a47821c80f1be7895df551ce392e048c6
xiamao <dingwenhao@bupt.edu.cn>
2018-03-09 20:50:39 +0800

    recover bst

--- Solutions/98_ValidateBinarySearchTree.py
+++ Solutions/98_ValidateBinarySearchTree.py
@@ -51,11 +51,29 @@ class Solution(object):
         li.append(root.val)
         self.inno_tree(root.right, li)
 
+    def recover_bst(self, root):
+        node_list = []
+        val_list = []
+        self.inorder(root, node_list, val_list)
+        val_list.sort()
+        for i, node in enumerate(node_list):
+            node.val = val_list[i]
+
+    def inorder(self, root, n_list, v_list):
+        if not root:
+            return
+        self.inorder(root.left, n_list, v_list)
+        v_list.append(root.val)
+        n_list.append(root)
+        self.inorder(root.right, n_list, v_list)
+
 
 if __name__ == '__main__':
     tree = TreeNode(2)
-    tree.left = TreeNode(1)
-    tree.right = TreeNode(3)
+    tree.left = TreeNode(3)
+    tree.right = TreeNode(1)
     so = Solution()
-    print so.isValidBST(tree)
-    print so.is_valid_bst_sec(tree)
+    # print so.isValidBST(tree)
+    # print so.is_valid_bst_sec(tree)
+    so.recover_bst(tree)
+    print tree.left.val
