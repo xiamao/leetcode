db4990d1c907c90dcace16c2ff3c20975a736e20
xiamao <dingwenhao@bupt.edu.cn>
2018-03-14 23:35:12 +0800

    max minimum depth of binary tree

--- Solutions/minium_depth_of_binary_tree.py
+++ Solutions/minium_depth_of_binary_tree.py
@@ -8,4 +8,11 @@ from leetcode_util.TreeNode import TreeNode
 
 class Solution(object):
     def min_depth(self, root):
+        if not root:
+            return 0
+        if not root.left:
+            return self.min_depth(root.right) + 1
+        if not root.right:
+            return self.min_depth(root.left) + 1
+        return min(self.min_depth(root.left), self.min_depth(root.right)) + 1
 
